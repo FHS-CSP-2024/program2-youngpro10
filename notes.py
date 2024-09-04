55633b6f531e818f8c940b68d880294af8e495ae
Saamynathan Yegapan <143142896+youngpro10@users.noreply.github.com>
2024-09-04 14:53:43 +0000

    I finish taking my notes now going to start porgram two

---
+++ notes.py
@@ -0,0 +1,15 @@
+name = input("Please tell me your name!")
+print("Hi " + name) 
+
+name = input("Give an another name  \n\t ")
+
+print("Hi "+ name)
+print(name + " Pretty cool name ")
+print("I wish my name was " + name)
+
+name = input("Tell me your name now! ")
+email = input("Or let me send spam to your email")
+nickname = input("What do your friends call you? ")
+
+print("Hey " + name +", or should I say" + nickname + ".")
+print("I am going send you to spam" + email )
